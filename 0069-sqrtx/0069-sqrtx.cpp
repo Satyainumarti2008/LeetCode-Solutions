f212e5de36b21b3b5803ec246f2780fd874fef4c
Satyainumarti2008 <satyainumarti2007@gmail.com>
2026-06-16 15:10:56 +0530

    Time: 0 ms (100%), Space: 8.4 MB (86.19%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        // int l=0,r=x;
-        // while(l<=r){
-        //     int mid=l+(r-l)/2;
-        //     if(sqrt(mis))
-        // }
         return sqrt(x);
     }
 };
