d66a0898b47a6c775d4c5d30f3e1baa382a848d3
Satyainumarti2008 <satyainumarti2007@gmail.com>
2026-06-20 09:59:42 +0530

    Time: 22 ms (6.68%), Space: 60.8 MB (18.76%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int find(int m,int d,vector<int>w){
         int s=0;
-        int c=0;
+        int c=1;
         for(int i=0;i<w.size();i++){
             if(s+w[i]<=m)
                 s+=w[i];
@@ -11,7 +11,7 @@ public:
                 s=w[i];
             }
         }
-        return c+1;
+        return c;
     }
     int shipWithinDays(vector<int>& weights, int days) {
         int maxs=-1;
