048156e340bd7219f6b15949c98aa0bce9d72e4d
Satyainumarti2008 <satyainumarti2007@gmail.com>
2026-06-30 12:09:53 +0530

    Time: 10 ms (5.08%), Space: 8.9 MB (1.71%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -4,8 +4,8 @@ public:
         int c=0;
         int temp=n;
         for(int i=1;i<=n;i++){
-            if(temp-i>=0){
-                temp-=i;
+            temp-=i;
+            if(temp>=0){
                 c++;
             }
             else 
