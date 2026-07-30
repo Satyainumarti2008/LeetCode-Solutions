58f044ed1c903399c02a579a9758a55805e9c479
Satyainumarti2008 <satyainumarti2007@gmail.com>
2026-07-30 21:41:28 +0530

    Time: 0 ms (100%), Space: 19.7 MB (9.07%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,30 +10,43 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode d(0);
-        ListNode* a=&d;
-        while(list1!=nullptr&&list2!=nullptr){
-            if(list1->val<=list2->val){
-                a->next=list1;
-                list1=list1->next;
-            }
-            else{
-                a->next=list2;
-                list2=list2->next;
-            }
-            a=a->next;
-        }
-            while(list1!=nullptr){
-                a->next=list1;
-                list1=list1->next;
-                a=a->next;
-            }
+    ListNode* merge(ListNode *ans,ListNode *list1,ListNode *list2){
+        if(list1==nullptr){
             while(list2!=nullptr){
-                a->next=list2;
+                ans->next=new ListNode(list2->val);
+                ans=ans->next;
                 list2=list2->next;
-                a=a->next;
             }
-        return d.next;
+            return ans;
+        }
+        if(list2==nullptr){
+            while(list1!=nullptr){
+                ans->next=new ListNode(list1->val);
+                ans=ans->next;
+                list1=list1->next;
+            }
+            return ans;
+        }
+        if(list1->val<=list2->val){
+            ans->next=new ListNode(list1->val);
+            ans=ans->next;
+            list1=list1->next;
+        }
+        else{
+            ans->next=new ListNode(list2->val);
+            ans=ans->next;
+            list2=list2->next;
+        }
+        
+        return merge(ans,list1,list2);
+    }
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *ans=new ListNode();
+        if(list1==nullptr)
+            return list2;
+        if(list2==nullptr)
+            return list1;
+        merge(ans,list1,list2);
+        return ans->next;
     }
 };
