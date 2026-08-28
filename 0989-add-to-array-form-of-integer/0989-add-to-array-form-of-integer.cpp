d8e8c791cdbf14aec15cab41c7ca581a19c0ae29
Satyainumarti2008 <satyainumarti2007@gmail.com>
2026-08-28 19:29:10 +0530

    Time: 0 ms (100%), Space: 31.5 MB (94.14%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
@@ -6,20 +6,20 @@ public:
             if(k > 0){
                 int t = num[i] + k % 10;
                 num[i] = (t + c) % 10;
-                c = (t+c) / 10;
+                c = (t + c) / 10;
                 k /= 10;
             }
             else{
                 int r = c + num[i];
                 c = r / 10;
-                num[i] = r%10;
+                num[i] = r % 10;
             }
         }
         while(k > 0){
             int t = k % 10 + c;
             c = t / 10;
-            num.insert(num.begin(),t%10);
-            k /=10;
+            num.insert(num.begin(),t % 10);
+            k /= 10;
         }
         if(c != 0){
             num.insert(num.begin(),c);
